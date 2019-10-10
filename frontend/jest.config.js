9df9e5515630fe09337abced31d590c6e6e7c6e2
Julien Londeix <jlondeix@gmail.com>
2019-10-10 10:28:55 +0200

    Initialized unit testing

---
+++ frontend/jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    moduleFileExtensions: [
+      "js"
+    ],
+    testMatch: [
+      "**/*.(test|spec).js"
+    ],
+    globals: {
+      "jest": {
+        useBabelrc: true,
+        tsConfigFile: "jest.config.js"
+      }
+    },
+    coveragePathIgnorePatterns: [
+      "/node_modules/",
+      "enzyme.js"
+    ],
+    setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
+    coverageReporters: [
+      "json",
+      "lcov",
+      "text",
+      "text-summary"
+    ],
+    moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/mocks.js",
+      "\\.(css|less|scss)$": "<rootDir>/__mocks__/mocks.js"
+    }
+  };
